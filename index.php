9b25f5444d321f27c6364b1146c4dea14d988f70
kalissyn <kalissyn@gmail.com>
2020-08-06 19:47:57 +0600

    Makers

---
+++ index.php
@@ -0,0 +1,533 @@
+
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- basic page needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Makers| Welcome</title>
+	<meta name="description" content="">  
+	<meta name="author" content="">
+
+   <!-- mobile specific metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+ 	<!-- CSS
+   ================================================== -->
+   <link rel="stylesheet" href="css/base.css">  
+   <link rel="stylesheet" href="css/main.css">
+   <link rel="stylesheet" href="css/vendor.css">     
+
+   <!-- script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- favicons
+	================================================== -->
+	<link rel="icon" type="image/png" href="favicon.png">
+
+</head>
+
+<body id="top">
+
+	<!-- header 
+   ================================================== -->
+   <header>
+
+   	<div class="row">
+
+   		<div class="logo">
+	         <a href="index.html">Makers Programming Course</a>
+	      </div>
+
+	   	<nav id="main-nav-wrap">
+				<ul class="main-navigation">
+					<li class="current"><a class="smoothscroll"  href="#intro" title="">Home</a></li>
+					<li><a class="smoothscroll"  href="#process" title="">About</a></li>
+					<li><a class="smoothscroll"  href="#features" title="">Apply now</a></li>
+					<li><a class="smoothscroll"  href="#pricing" title="">Pricing</a></li>
+					<li><a class="smoothscroll"  href="#faq" title="">FAQ</a></li>					
+					<li class="highlight with-sep"><a href="#" title="">Sign Up</a></li>					
+				</ul>
+			</nav>
+
+			<a class="menu-toggle" href="#"><span>Menu</span></a>
+   		
+   	</div>   	
+   	
+   </header> <!-- /header -->
+
+	<!-- intro section
+   ================================================== -->
+   <section id="intro">
+
+   	<div class="shadow-overlay"></div>
+
+   	<div class="intro-content">
+   		<div class="row">
+
+   			<div class="col-twelve">
+
+	   			<div class='video-link'>
+	   				<a ><img src="images/play-button.png" alt=""></a>
+	   			</div>
+
+	   			<h5>Hello, welcome to Makers Programming School</h5>
+	   			<h1>Lauch your tech careeer with Makers</h1>
+
+	   			<a class="button stroke smoothscroll" href="#process" title="">Learn More</a>
+
+	   		</div>  
+   			
+   		</div>   		 		
+   	</div> 
+ 	
+
+   </section> <!-- /intro -->
+
+
+   <!-- Process Section
+   ================================================== -->
+   <section id="process">  
+
+   	<div class="row section-intro">
+   		<div class="col-twelve with-bottom-line">
+
+   			
+   			<h1>About us</h1>
+
+   			<p class="lead">At Makers, we are creating a new generation of tech talent who are skilled and ready for the changing world of work. We are inspired by the idea of discovering and unlocking potential in people for the benefit of 21st century business and society.</p>
+
+   		</div>   		
+   	</div>
+
+   	<!-- <div class="row process-content">
+
+   		<div class="left-side">
+
+
+
+   		</div> /right-side -->  
+
+   		<div class="image-part"></div>  			
+
+   	</div> 
+
+   </section> <!-- /process-->    
+
+
+   <!-- features Section
+   ================================================== -->
+	<section id="features">
+
+		<div class="row section-intro">
+   		<div class="col-twelve with-bottom-line">
+
+   			<h5>Community</h5>
+   			<h1>Once a Maker, always a Maker</h1>
+
+   			<p class="lead">Our virtual community is there to hold you accountable and support you through this intensive, transformative experience, and then, to help you in your new field as a professional network.</p>
+
+   		</div>   		
+   	</div>
+
+   	<div class="row features-content">
+
+   		<div class="features-list block-1-3 block-s-1-2 block-tab-full group">
+
+	      	<div class="bgrid feature">	
+
+	      		<span class="icon"><i class="icon-window"></i></span>            
+
+	            <div class="service-content">	
+
+	            	 <h3 class="h05">Learn with the support of our buzzing community</h3>
+
+		            <p>The supportive learning environment we cultivate at Makers is fundamental to the success of our developers, and it’s just as effective regardless of whether the cohort community is on slack, on zoom, or onsite.
+	         		</p>
+	         		
+	         	</div> 	         	 
+
+				</div> <!-- /bgrid -->
+
+				<div class="bgrid feature">	
+
+					<span class="icon"><i class="icon-eye"></i></span>                          
+
+	            <div class="service-content">	
+	            	<h3 class="h05">Pair programming daily</h3>  
+
+		            <p>Pairing helps you learn because it gives you a structure to communicate. By practicing how to explain new concepts to one another you’ll accelerate your learning and develop the essential collaboration skills you’ll need in a modern software environment.
+	         		</p>
+
+	         		
+	            </div>	                          
+
+			   </div> <!-- /bgrid -->
+
+			   <div class="bgrid feature">
+
+			   	<span class="icon"><i class="icon-paint-brush"></i></span>		            
+
+	            <div class="service-content">
+	            	<h3 class="h05">Alumni forum</h3>
+
+		            <p>Our alumni community is engaged on our alumni forum, they attend networking events, and upskilling trainings Makers organises for them.
+When you join Makers, you join a community of developers who are already working in tech. It’s not just a course: it’s a professional network.
+	        			</p> 
+
+	        			
+	            </div> 	            	               
+
+			   </div> <!-- /bgrid -->
+
+	      </div> <!-- features-list -->
+   		
+   	</div> <!-- features-content -->
+		
+	</section> <!-- /features -->
+	
+
+	<!-- pricing
+   ================================================== -->
+
+   <section id="pricing">
+
+   	<div class="row section-intro">
+   		<div class="col-twelve with-bottom-line">
+
+   			<h5>Our Pricing</h5>
+   			<h1>Pick the best plan for you.</h1>
+
+   			
+
+   		</div>   		
+   	</div>
+
+   	<div class="row pricing-content">
+
+         <div class="pricing-tables block-1-4 group">
+
+            <div class="bgrid"> 
+
+            	<div class="price-block">
+
+            		<div class="top-part">
+
+	            		<h3 class="plan-title">Starter</h3>
+		               <p class="plan-price"><sup>$1000</sup></p>
+		               <p class="price-month">16 weeks</p>
+	            	</div>                
+					<div class="bottom-part">
+
+					<ul class="features">
+
+					<li>Mobile Development</li>		                  
+					<li> Front-End Development</li>		                  
+					<li></li>
+					</ul>
+
+	               <a class="button large" href="">Get Started</a>
+
+	            	</div>
+
+            	</div>           	
+                        
+			   </div> <!-- /price-block -->
+
+            <div class="bgrid">
+
+            	<div class="price-block primary">
+
+            		<div class="top-part" data-info="recommended">
+
+	            		<h3 class="plan-title">Tech Academy</h3>
+		               <p class="plan-price"><sup>$</sup>1800</p>
+		               <p class="price-month">24 weeks</p>
+	            	</div>               
+
+	               <div class="bottom-part">
+
+	            		<ul class="features">
+
+		                  <li>Web Design (UX/UI)</li>		                  
+		                  <li> Front-End Development</li>		                  
+		                  <li>	Full-Stack Web Development</li>
+		               </ul>
+
+		               <a class="button large" href="">Get Started</a>
+
+	            	</div>
+            		
+            	</div>            	                 
+
+			  </div> <!-- /price-block -->
+
+           <div class="bgrid">               
+
+               <div class="price-block">
+
+            		<div class="top-part">
+
+	            		<h3 class="plan-title">Premium</h3>
+		               <p class="plan-price"><sup>$</sup>2800</p>
+		               <p class="price-month">20 weeks</p>
+		            		               
+
+	            	</div> 
+	            	
+						<div class="bottom-part">
+
+
+	            		<ul class="features">
+		                  <li>Web Design (UX/UI)</li>		                  
+		                  <li> Front-End Development</li>		                  
+						  <li>	Full-Stack Web Development</li>
+						  <li> Product Management</li>		                  
+		                  <li> Data Science</li>
+		               </ul>
+
+
+		               <a class="button large" href="">Get Started</a>
+
+	            	</div>	            		                
+            		
+            	</div>                              
+
+			   </div> <!-- /price-block --> 
+
+			  
+
+			   </div> <!-- /price-block -->           
+
+         </div> <!-- /pricing-tables --> 
+
+      </div> <!-- /pricing-content --> 
+
+   </section> <!-- /pricing --> 
+
+
+   <!-- Testimonials Section
+   ================================================== -->
+   <section id="testimonials">
+
+   	<div class="row">
+   		<div class="col-twelve">
+   			<h2 class="h01">Hear What Are Students Say.</h2>
+   		</div>   		
+   	</div>   	
+
+      <div class="row flex-container">
+    
+         <div id="testimonial-slider" class="flexslider">
+
+            <ul class="slides">
+
+               <li>
+               	<div class="testimonial-author">
+                    	<img src="images/avatars/avatar-1.jpg" alt="Author image">
+                    	<div class="author-info">
+                    	Mark Luxe
+                    
+                    	</div>
+                  </div>
+
+                  <p>
+				  “I have always tended to enjoy working alone more, so mandatory pairing was a big shift in learning style for me. But I learnt so much from other cohort members, or by explaining my understanding to other cohort members.”  						
+                  </p>                  
+             	</li> <!-- /slide -->
+
+               <li> 
+
+               	<div class="testimonial-author">
+                    	<img src="images/avatars/avatar-2.jpg" alt="Author image">
+                    	<div class="author-info">
+                    		John Doe
+                    		
+                    	</div>
+                  </div> 
+
+                  <p>
+                  "This is the best course and support from the other cohort members was so important, I feel we were all there for each other and that helped a lot.”  
+                  </p>
+                                         
+               </li> <!-- /slide -->
+
+            </ul> <!-- /slides -->
+
+         </div> <!-- /testimonial-slider -->         
+        
+      </div> <!-- /flex-container -->
+
+   </section> <!-- /testimonials -->
+
+
+   <!-- faq
+   ================================================== -->
+   <section id="faq">
+
+   	<div class="row section-intro">
+   		<div class="col-twelve with-bottom-line">
+
+   			<h5>Faq</h5>
+   			<h1>Questions and Answers.</h1>
+
+   		</div>   		
+   	</div>
+
+   	<div class="row faq-content">
+
+   		<div class="q-and-a block-1-2 block-tab-full group">
+
+   			<div class="bgrid">
+
+   				<h3>What are the requirements to get on the Makers course?</h3>
+
+   				<p>Makers teaches the course from beginner level. However, before someone starts the course, we ask them to do some free, online courses.</p>
+
+   			</div>
+
+   			<div class="bgrid">
+
+   				<h3>How does career support at Makers work?</h3>
+
+   				<p>Makers has the largest dedicated career coaching team in the industry providing 1:1 support to help you launch your tech career.
+					   We will provide comprehensive and personalised career support to each and every one of our developers.
+We'll do everything in our power to help you get your first job..</p>
+
+   			</div>
+
+   			<div class="bgrid">
+
+   				<h3>What resources do you recommend?</h3>
+
+   				<p>There are loads of resources online which can help you brush up on your skills before you apply however, we recommend the following resources to get started on your journey.
+					   CodeAcademy, PairUp, Learn Ruby in 15.
+				   </p>
+
+   			</div>
+
+   			<div class="bgrid">
+
+   				<h3>How much programming do I need to know to join?</h3>
+
+   				<p> We don't expect you to know all there is to know about coding but you'll need to demonstrate that you're serious about applying and have taken steps to begin your educational journey</p>
+
+   			</div>
+
+
+   		</div> <!-- /q-and-a --> 
+   		
+   	</div> <!-- /faq-content --> 
+
+
+
+   </section> <!-- /faq --> 
+
+  
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+   	<div class="footer-main">
+
+   		<div class="row">  
+
+
+
+	
+		      </div> <!-- /footer-info -->
+
+	      	<div class="col-two tab-1-3 mob-1-2 site-links">
+
+	      		<h4>Site Links</h4>
+
+	      		<ul>
+	      			<li><a href="#">About Us</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">FAQ</a></li>
+					</ul>
+
+	      	</div> <!-- /site-links -->  
+
+	      	<div class="col-two tab-1-3 mob-1-2 social-links">
+
+	      		<h4>Social</h4>
+
+	      		<ul>
+	      			<li><a href="#">Twitter</a></li>
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Dribbble</a></li>
+						<li><a href="#">Google+</a></li>
+						<li><a href="#">Skype</a></li>
+					</ul>
+	      	           	
+	      	</div> <!-- /social --> 
+
+	      	<div class="col-four tab-1-3 mob-full footer-subscribe">
+
+	      		<h4>Subscribe</h4>
+
+	      		<p>Keep yourself updated. Subscribe to our newsletter.</p>
+
+	      		<div class="subscribe-form">
+	      	
+	      			<form id="mc-form" class="group" novalidate="true">
+
+							<input type="email" value="" name="dEmail" class="email" id="mc-email" placeholder="type email &amp; hit enter" required=""> 
+	   		
+			   			<input type="submit" name="subscribe" >
+		   	
+		   				<label for="mc-email" class="subscribe-message"></label>
+			
+						</form>
+
+	      		</div>	      		
+	      	           	
+	      	</div> <!-- /subscribe -->         
+
+	      </div> <!-- /row -->
+
+   	</div> <!-- /footer-main -->
+
+
+      <div class="footer-bottom">
+
+      	<div class="row">
+
+      		<div class="col-twelve">
+	      		<div class="copyright">
+		         	<span>© Copyright Makers 2016.</span> 
+		              	
+		         </div>
+
+		         <div id="go-top" style="display: block;">
+		            <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon ion-android-arrow-up"></i></a>
+		         </div>         
+	      	</div>
+
+      	</div> <!-- /footer-bottom -->     	
+
+      </div>
+
+   </footer>  
+
+   <div id="preloader"> 
+    	<div id="loader"></div>
+   </div> 
+
+   <!-- Java Script
+   ================================================== --> 
+   <script src="js/jquery-1.11.3.min.js"></script>
+   <script src="js/jquery-migrate-1.2.1.min.js"></script>
+   <script src="js/plugins.js"></script>
+   <script src="js/main.js"></script>
+
+</body>
+
+</html>
